6cb073b43d0be2636dbdf8ee4953fdb51c62cd90
Ted (Pololu) <dev-ted@pololu.com>
2014-08-14 13:30:03 -0700

    removed custom pin initializing, converted tabs to spaces, set default values for flipped status flags

--- DRV8835MotorShield.h
+++ DRV8835MotorShield.h
@@ -1,29 +1,28 @@
-#ifndef DualMotorShield_h
-#define DualMotorShield_h
+#ifndef DRV8835MotorShield_h
+#define DRV8835MotorShield_h
 
 #include <Arduino.h>
 
 class DualMotorShield
 {
-	public:
-		DualMotorShield();
-		DualMotorShield(unsigned char M1DIR, unsigned char M1PWM, unsigned char M2DIR, unsigned char M2PWM);
-	
-		void setM1Speed(int speed);
-		void setM2Speed(int speed);
-		void setSpeeds(int M1, int M2);
-		
-		void flipM1(bool flip);
-		void flipM2(bool flip);
-	
-	private:
-		void init();
-		unsigned char _M1DIR;
-		unsigned char _M2DIR;
-		unsigned char _M1PWM;
-		unsigned char _M2PWM;
-		bool flippedM1;
-		bool flippedM2;
+  public:
+    DualMotorShield();
+  
+    void setM1Speed(int speed);
+    void setM2Speed(int speed);
+    void setSpeeds(int m1Speed, int m2Speed);
+    
+    void flipM1(bool flip);
+    void flipM2(bool flip);
+  
+  private:
+    void init();
+    unsigned char _M1DIR;
+    unsigned char _M2DIR;
+    unsigned char _M1PWM;
+    unsigned char _M2PWM;
+    bool flippedM1 = false;
+    bool flippedM2 = false;
 };
 
 
