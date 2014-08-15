f638fa1501e521410c4921f35f5db93f375c0af0
RyanTM (Pololu) <dev-ryantm@pololu.com>
2014-08-15 09:16:36 -0700

    make demo more like ZumoMotors demo, add comments

--- DRV8835MotorShield/examples/DRV8835MotorShieldDemo/DRV8835MotorShieldDemo.ino
+++ DRV8835MotorShield/examples/DRV8835MotorShieldDemo/DRV8835MotorShieldDemo.ino
@@ -1,15 +1,30 @@
 #include <DRV8835MotorShield.h>
 
+/*
+ * This example uses the DRV8835MotorShield library to drive each motor with the
+ * Pololu DRV8835 Dual Motor Driver Shield for Arduino forward, then backward. 
+ * The yellow user LED is on when a motor is set to a positive speed and off when
+ * a motor is set to a negative speed.
+ */
+
+#define LED_PIN 13
 
 DRV8835MotorShield motors;
 
 void setup()
 {
+  pinMode(LED_PIN, OUTPUT);
+  
+  // uncomment one or both of the following lines if your motors' directions need to be flipped
+  //motors.flipM1(true);
+  //motors.flipM2(true);
 }
 
 void loop()
 {
-  // run left motor forward
+  // run M1 motor with positive speed
+  
+  digitalWrite(LED_PIN, HIGH);
   
   for (int speed = 0; speed <= 400; speed++)
   {
@@ -23,7 +38,9 @@ void loop()
     delay(2);
   }
   
-  // run left motor backward
+  // run M1 motor with negative speed
+  
+  digitalWrite(LED_PIN, LOW);
   
   for (int speed = 0; speed >= -400; speed--)
   {
@@ -37,8 +54,9 @@ void loop()
     delay(2);
   }
 
-  // run right motor forward
-
+  // run M2 motor with positive speed
+  
+  digitalWrite(LED_PIN, HIGH);
   
   for (int speed = 0; speed <= 400; speed++)
   {
@@ -52,7 +70,9 @@ void loop()
     delay(2);
   }
   
-  // run right motor backward
+  // run M2 motor with negative speed
+  
+  digitalWrite(LED_PIN, LOW);
   
   for (int speed = 0; speed >= -400; speed--)
   {
